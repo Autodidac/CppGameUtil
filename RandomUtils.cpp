fbef0e331c2cefb6ee995f117dc39b8b04f23161
Autodidac <177971875+Autodidac@users.noreply.github.com>
2024-08-24 06:47:17 -0400

    Create RandomUtils.cpp

---
+++ RandomUtils.cpp
@@ -0,0 +1,19 @@
+#include "RandomUtils.h"
+#include "pch.h"
+
+namespace Utilities {
+
+    int RandomUtils::getRandomInt(int min, int max) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(min, max);
+        return dis(gen);
+    }
+
+    double RandomUtils::getRandomDouble(double min, double max) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(min, max);
+        return dis(gen);
+    }
+}
